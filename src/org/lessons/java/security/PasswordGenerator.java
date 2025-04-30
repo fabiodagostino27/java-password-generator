6099a73e729c7b83c8201a0e8b9bc5c76332de48
fabiodagostino27 <fabiodago27@gmail.com>
2025-04-30 17:30:00 +0200

    ADD: password print

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -6,11 +6,19 @@ public class PasswordGenerator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.println("Scrivi il tuo nome");
         String nome = in.nextLine();
+        System.out.println("Scrivi il tuo cognome");
         String cognome = in.nextLine();
+        System.out.println("Scrivi il tuo colore preferito");
         String colore = in.nextLine();
+        System.out.println("Scrivi il giorno della tua data di nascita");
         int giorno = in.nextInt();
+        System.out.println("Scrivi il mese della tua data di nascita");
         int mese = in.nextInt();
+        System.out.println("Scrivi l'anno della tua data di nascita");
         int anno = in.nextInt();
+
+        System.out.println("Ecco la tua password: " + nome + "-" + cognome + "-" + colore + "-" + (giorno + mese + anno));
     }
 }
