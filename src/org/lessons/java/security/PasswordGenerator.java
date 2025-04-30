0ac64ac2cf492fd4232ca418849c19f42febe31f
fabiodagostino27 <fabiodago27@gmail.com>
2025-04-30 17:21:23 +0200

    ADD: scanner import

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,9 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
     }
 }
