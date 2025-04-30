87e9fffcd0f054ba176cc682e4e71f7fc98b65c5
fabiodagostino27 <fabiodago27@gmail.com>
2025-04-30 17:23:29 +0200

    ADD: variables initialization

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -5,5 +5,12 @@ import java.util.Scanner;
 public class PasswordGenerator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        String nome = in.nextLine();
+        String cognome = in.nextLine();
+        String colore = in.nextLine();
+        int giorno = in.nextInt();
+        int mese = in.nextInt();
+        int anno = in.nextInt();
     }
 }
